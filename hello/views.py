85438778ab32231a44a9ebf0e22312c33d22ae89
BlissChen <hello19881990@gmail.com>
2018-03-24 16:30:54 +0800

    test

--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import time
 
 def hello(request):
+    for i in range(10):
+        print(i)
+        time.sleep(1)
     return HttpResponse('hello Django')
